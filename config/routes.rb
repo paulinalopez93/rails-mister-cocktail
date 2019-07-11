e1326f509251efbfc777bb85947ddcb428260066
Paulina Lopez <paulielopez93@gmail.com>
2019-07-11 10:00:40 -0500

    been doing the views for my cocktails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
+    resources :doses, only: %I[new create]
+  end
+  resources :doses, only: :destroy
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Ingredient.delete.all
+require 'json'
+require 'open-uri'
+require 'faker'
+Cocktail.delete_all
+Ingredient.delete_all
+
+puts 'Clearing database...'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_json = open(url).read
+ingredient_hash = JSON.parse(ingredient_json)
+
+drinks_array = ingredient_hash['drinks']
+
+drinks_array.each do |drink|
+  Ingredient.create(name: drink['strIngredient1'])
+end
+
+puts "Seed successful!"
+
+puts 'Creating cocktails...'
+
+10.times do
+  puts "Creating a cocktail"
+  p Cocktail.create!(
+    name: Faker::Cannabis.strain)
+end
+
+puts "finished!"
